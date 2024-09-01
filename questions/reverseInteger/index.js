64ca702bd36719b18f33e5871fc36d97d03d35ec
Brian <briancmuhammad@yahoo.com>
2024-09-01 08:27:07 -0700

    Finished reversed Integer challenge.

--- questions/reverseInteger/index.js
+++ questions/reverseInteger/index.js
@@ -7,8 +7,37 @@
  * reverseInt(-50) === -5
  */
 
-const reverseInteger = (int) => {
+//const reverseInteger = (int) => {
+//turn number into string then to array
+//return int.toString().split("").reverse().join("") // this does not pass though. all test fail
+//need to turn into number
+// return parseInt( int.toString().split("").reverse().join("")); // this does not pass though. only 2 out of three pass.
+//needs to be turned into negativ
 
-};
+//better but needs to be cleaned up
+//const reversed =  parseInt( int.toString().split("").reverse().join(""));
+
+//if (int < 0){
+//  return reversed * -1;
+//}
+//return reversed;
+//};
+
+///turn to ternary operator. can make better
+
+//const reverseInteger = (int) => {
+ // const reversed = parseInt(int.toString().split("").reverse().join(""));
+
+ // return int < 0 ? reversed * -1 : reversed;
+//};
+
+
+//cleaned up better can make one line but not readable good this way
+const reverseInteger = (int) => {
+    const reversed = int.toString().split("").reverse().join("");
+  
+    return parseInt(reversed) * Math.sign(int)
+  };
+  
 
 module.exports = reverseInteger;
