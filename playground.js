c953951cd9dbb9974a85d836f0d54060f3d4e156
Brian <briancmuhammad@yahoo.com>
2024-09-01 11:21:48 -0700

    Finished Max Char challenged. Use new Map() data strucutre best top tech

--- playground.js
+++ playground.js
@@ -1,30 +1,78 @@
-const arr = [1, -1, 2, 3];
-
-
+//const arr = [1, -1, 2, 3];
 
 //optimized by removing the second arguement.This would start at the first element in array
 // a = 1, c = -1 => a = 0
 // a = 0, c = 2 => a = 2
 // a = 2, c = 3 => a = 5
 
-//const sum = arr.reduce((accumulator, currentValue) => {  
- //   return accumulator + currentValue;
- //   })
+//const sum = arr.reduce((accumulator, currentValue) => {
+//   return accumulator + currentValue;
+//   })
 // can make one line
- const sum = arr.reduce((accumulator, current) => accumulator + current);
+//const sum = arr.reduce((accumulator, current) => accumulator + current);
 
 // a = 0, c = 1 => a = 1
 // a = 1, c = -1 => a = 0
 // a = 0, c = 2 => a = 2
 // a = 2, c = 3 => a = 5
-//const sum = arr.reduce((accumulator, currentValue) => {  
+//const sum = arr.reduce((accumulator, currentValue) => {
 //return accumulator + currentValue;
 //}, 0)
 //let sum = 0
 //for(let elem of arr) {
- //   sum += elem;
+//   sum += elem;
 //}
 
 // console.log('accumulator', accumulator);
 //console.log(`a = ${accumulator}, c = ${currentValue} => a =  ${accumulator + currentValue}`)
-console.log(sum);
+//console.log(sum);
+
+///character map example
+const str = "Simplified !";
+//const charMap = {};
+
+//for (let char of str) {
+  //console.log(char)
+  // charMap[char] = 1;// just maps to 1 { S: 1, i: 1, m: 1, p: 1, l: 1, f: 1, e: 1, d: 1, ' ': 1, '!': 1 }
+  //used if to check if character was seen before. needs optimization though
+  // if (!charMap[char]) {
+  // charMap[char] = 1;
+  // }
+  // else {
+  // charMap[char] = charMap[char] + 1; //sets to naan  need to add condition to check
+  //  }
+
+  //optimized
+ // for (let char of str) {
+ //   charMap[char] = charMap[char] + 1 || 1;
+ // }
+//}
+
+//even better way for top tech
+const charMap = new Map(); //Data structure to map 1 value to another. Map data structure made to be iterated over. can create a for loop
+
+//can change let to const bc it is not changing. best practice
+for (let char of str) {
+    //to get the count of times appearing for a map
+    const count = charMap.get(char)
+    charMap.set(char, count + 1 || 1);
+}
+
+//for (let keyAndValue of charMap) {
+    //const key = keyAndValue[0];
+ //   const [key, value] = keyAndValue;
+    //const value = keyAndValue[1]
+   // console.log(keyAndValue);
+ //  console.log(key, value);
+//}
+
+// in one line of code we can get key and value
+for (let [key, count] of charMap) {
+    console.log(key, count);
+}
+
+//const arr = [1, 2, 3]
+//in es6 i can get key and value faster using. destructiong
+//const [first, second] = arr
+//console.log(first, second) will yield 1 2
+//console.log(charMap);
--- questions/maxChar/index.js
+++ questions/maxChar/index.js
@@ -1,4 +1,4 @@
-/** Given a string, find the character that is used the 
+/** Given a string, find the character that is used the
  * maximum number of times.
  *
  * @example
@@ -6,8 +6,53 @@
  * maxCharacter('foo 111123') === '1'
  */
 
+//Common Questions
+//Which character appears the most amount of times?  **uses a character map**
+//Does string A contain the same characters as string B(anagram) create 2 character maps and check each character against each other
+//Are there any duplicate characters character map ...loop through
+
 const maxCharacter = (str) => {
-  
+  let maxChar = "";
+  let max = 0;
+
+  //Create a character map
+  //const charMap = {};
+  //for (let char of str) {
+  //  charMap[char] = charMap[char] + 1 || 1;
+  // }
+
+  //Loop over every character in the character map
+  //If the value of the character is greater than max
+  //set max to the value
+  //set maxChar to the character
+
+  // for (let char in charMap) {
+  // if (charMap[char] > max) {
+  //   max = charMap[char];
+  //   maxChar = char;
+  // }
+  //}
+
+  //best way for top tech use map data structure
+  const charMap = new Map();
+
+  for (const char of str) {
+    //to get the count of times appearing for a map
+    //const count = charMap.get(char);
+   // charMap.set(char, count + 1 || 1);
+   //const count = charMap.get(char)
+   charMap.set(char, charMap.get(char) + 1 || 1);
+  }
+
+  for (const [char, count] of charMap) {
+    if (count > max) {
+      max = count;
+      maxChar = char;
+    }
+  }
+
+  //return maxChar
+  return maxChar;
 };
 
 module.exports = maxCharacter;
