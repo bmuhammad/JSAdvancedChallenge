da5d3bc15a5a6820357cc9c2b36400792da264e3
Brian <briancmuhammad@yahoo.com>
2024-09-01 09:07:11 -0700

    Finished stringfy challenge. used set for unique ids.

--- questions/stringifyId/index.js
+++ questions/stringifyId/index.js
@@ -16,8 +16,62 @@
  * ]) === 'ABC, abc, CBA'
  */
 
-const stringifyId = (arr) => {
+//approach
+//Convert the Array of Objects into an Array of Id's
+//convert the array of Id's into an Array of unique ID's
+//Join the array to create a string of ID's separated by ","
 
+//Brute force
+//const stringifyId = (arr) => {
+//const idArray = arr.map(obj => obj.id);
+//const uniqueArray = [];
+// let idString = "";
+
+//removes duplicates. makes unique ids.
+//for (let id of idArray) {
+//   if (!uniqueArray.includes(id)) {
+//   uniqueArray.push(id);
+// }
+// }
+
+//need commas
+//for (let i = 0; i < uniqueArray.length; ++i) {
+//if we are not on last element
+//  if (i !== uniqueArray.length - 1) {
+//   idString += `${uniqueArray[i]}, `
+// }
+//  else {
+//   idString += uniqueArray[i];
+//  }
+//. add the id and a comma else just add the id
+//}
+
+//return idString;
+//}
+
+//better solution but not the best
+//const stringifyId = (arr) => {
+//const idArray = arr.map(obj => obj.id);
+//const uniqueArray = [];
+
+//for (let id of idArray) {
+//   if (!uniqueArray.includes(id)) {
+//   uniqueArray.push(id);
+// }
+// }
+
+//  return uniqueIdArray.join(", ");
+// };
+
+//top tech
+//can decease most of the lines above
+const stringifyId = (arr) => {
+  const idArray = arr.map((obj) => obj.id);
+
+  //set operator converts to unique id ... is a spread syntax
+  const uniqueIdArray = [...new Set(idArray)];
+
+  return uniqueIdArray.join(", ");
 };
 
 module.exports = stringifyId;
